54d651bb09afbb7be4c7603f38197eb189b04324
MoElkilany <mohamedsayedelkilany@gmail.com>
2021-09-29 23:03:34 +0200

    install FS Calendar

---
+++ FSCalender-Demo/Core/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  FSCalender-Demo
+//
+//  Created by Mohamed Elkilany on 29/09/2021.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = ViewController()
+        window?.makeKeyAndVisible()
+        return true
+    }
+}
+
